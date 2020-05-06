8916d40552b21a44496c5a6d903424989b2d295c
Nadine Ju <nadine.jura@online.de>
2020-05-05 21:36:21 -0700

    Course model cleanup function created

---
+++ models/Course.js
@@ -0,0 +1,38 @@
+const coursesCollection = require("../db").db().collection("courses");
+
+let Course = function(data) {
+    this.data = data;
+    // this.errors = [];
+};
+
+Course.prototype.cleanup = function() {
+    if (typeof(this.data.name != "string")) {
+        this.data.name = "";
+    };
+    if (typeof(this.data.instructor != "string")) {
+        this.data.instructor = "";
+    };
+    if (typeof(this.data.platform != "string")) {
+        this.data.platform = ""; 
+    };
+
+    this.data = {
+        name: this.data.name.trim(),
+        instructor: this.data.instructor.trim(),
+        platform: this.data.platform.trim(),
+        keywords: [],
+        progress: 0,
+        time: 0,
+        completed: false
+    };
+};
+
+Course.prototype.create = function() {
+    return new Promise(async (resolve, reject) => {
+        this.cleanup();
+        await coursesCollection.insertOne(this.data);
+        resolve();
+    });
+};
+
+module.exports = Course;
