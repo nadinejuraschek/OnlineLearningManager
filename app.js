db2ef8a19b89dd55a1052b7fda843d83d56bd617
Nadine Ju <nadine.jura@online.de>
2020-05-06 19:07:21 -0700

    Login styling

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.use('*', router);
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
 
 app.listen(process.env.PORT, function() {
     console.log(`Server has started on ${process.env.PORT}.`);
